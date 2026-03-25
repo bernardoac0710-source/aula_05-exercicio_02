edf90c3dd8ed3ca555fa2550109fa069097e29db
Bernardo Andrade de Castro <1261949045@ulife.com.br>
2026-03-24 21:01:29 -0300

    exercicio 2

---
+++ src/App.java
@@ -0,0 +1,31 @@
+import java.util.Scanner; 
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("digite o primeiro lado da piramide: ");
+        int ladoA = entrada.nextInt();
+
+        System.out.print("digite o segundo lado da piramide: ");
+        int ladoB = entrada.nextInt();
+
+        System.out.print("digite o terceiro lado da piramide: ");
+        int ladoC = entrada.nextInt();
+
+
+        if (ladoA == ladoB && ladoB == ladoC) {
+            System.out.print("essa é uma piramide equilatero.");
+        }
+            else if (ladoA != ladoB && ladoB != ladoC) {
+                System.out.print("essa é uma piramide escaleno. ");
+                
+             } 
+             else {
+                System.out.print("essa é uma piramide isosceles. ");
+                
+            }
+            
+        
+        entrada.close();
+    }
+}
